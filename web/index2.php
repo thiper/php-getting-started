9b5ebb983b05647c975a0297b4af97ed86f8b72a
thiper <contato@thiper.com>
2022-04-16 11:04:49 -0400

    atualizacao

--- web/index2.php
+++ web/index2.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Origin: http://localhost/");
+//header("Access-Control-Allow-Origin: http://localhost/");
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header('Content-Type: application/json');
