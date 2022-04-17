231660c21bf442ac3c4e52cc499bd7ed57385187
thiper <contato@thiper.com>
2022-04-17 09:38:08 -0400

    atualizar

--- web/time.php
+++ web/time.php
@@ -1,7 +1,7 @@
 <?php
 $date = date_create();
 //echo date_format($date, 'U = Y-m-d H:i:s') . "\n";
-echo date_format($date, 'Y-m-d H:i:s') . "\n";
+echo date_format($date, 'y-m-d H:i:s') . "\n";
 
 
 
